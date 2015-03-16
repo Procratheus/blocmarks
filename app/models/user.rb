fe4ae3dae025df9cb96fbc4acaa9ddf7b9ae8fad
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-03-16 23:36:33 +0000

    omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable,
-         :omniauthable, :omniauth_providers => [:twitter, :facebook]
+         :omniauthable, :omniauth_providers => [:facebook]
+
+  # omniauth controller method
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+    end
+  end
 end

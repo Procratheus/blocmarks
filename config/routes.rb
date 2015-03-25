34b8f19fc5f81dcb28ada340e2d9edc9ca2567d7
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-03-25 20:29:17 +0000

    updated incoming controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks"}
+  resources :bookmarks
   post :incoming, to: "incoming#create"
   root "welcome#index"
 end

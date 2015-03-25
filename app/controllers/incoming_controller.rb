97794c1538a7f0757d5b061348a6ec1798cea130
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-03-25 19:49:06 +0000

    updated incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,6 +5,22 @@ class IncomingController < ApplicationController
   def create
     puts "INCOMING PARAMS HERE: #{params}"
 
+    @user = User.find_by(email: params[:sender])
+    @topic = Topic.find_by(title: params[:subject])
+    @body = params["body-plain"]
+
+    if @user
+      if @topic
+        @subject = @topic.bookmarks.build(url: @body)
+        @subject.save
+      else
+        @subject = @user.topics.build(title: params[:subject])
+        @subject.save
+        @bookmark = @subject.bookmarks.build(url: @body)
+        @bookmark.save
+      end
+    end
+
     head 200
   end
 

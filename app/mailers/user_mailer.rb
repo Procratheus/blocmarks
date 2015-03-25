4c8a147ea02aa8e39c2eed6764ddec0a54a32f13
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-03-25 00:03:51 +0000

    mailgun integration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,11 @@
 class UserMailer < ApplicationMailer
-  
+ 
+  def receive(email)
+    page = Page.find_by(address: email.to.first)
+    page.emails.create(
+      subject: email.subject,
+      body: email.body
+    )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks"}
+  post :incoming, to: "incoming#create"
   root "welcome#index"
 end

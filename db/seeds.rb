7fe339725438321c98229f63773849e30ea1a699
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-04-01 21:08:30 +0100

    embedly

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "faker"
+
+User.delete_all
+Topic.delete_all
+Bookmark.delete_all
+
+#Create Users
+5.times do 
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(8...20)
+    )
+  user.save!
+end
+
+users = User.all
+
+#Create Topics
+10.times do 
+  topic = Topic.new(
+    user_id: users.sample,
+    title: Faker::Lorem.word
+    )
+  topic.save!
+end
+
+topics = Topic.all
+
+#Create Bookmarks
+20.times do 
+  bookmark = Bookmark.new(
+    topic_id: topics.sample,
+    url: Faker::Internet.url
+    )
+  bookmark.save!
+end
+
+bookmarks = Bookmark.all
+
+puts "Seeds Finished"
+puts "#{users.all.count} users created"
+puts "#{topics.all.count} topics created"
+puts "#{bookmarks.all.count} bookmarks created"
+
+
+

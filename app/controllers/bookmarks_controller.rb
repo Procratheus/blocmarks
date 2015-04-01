58041744283a55b28625182fb0b4682bc6182de2
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-04-01 20:33:52 +0100

    latest

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,43 @@
 class BookmarksController < ApplicationController
+  before_action :set_topic
+  before_action :set_bookmark, except: [:create, :new]
+
+  def show
+    
+  end
+
   def new
+    
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.build(bookmark_params)
   end
 
   def edit
+    
   end
 
-  def show
+  def update
+    
   end
 
-  def index
+  def destroy
+    
+  end
+
+  protected
+
+  def bookmark_params
+    params.require(:bookmark).permit(:title, :url)
+  end
+
+  def set_topic
+    @topic = Topic.find(params[:topic_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 end

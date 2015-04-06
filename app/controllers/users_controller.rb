8c9dfba1da75fea475980402decea85821303580
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-04-06 13:43:30 +0100

    user profile set-up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+
+  def show
+    @user_bookmarks =
+    @liked_bookmarks = 
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks"}
+  resources :users, only: [:show]
   resources :topics do
     resources :bookmarks do 
       resources :likes, only: [:create, :destroy]

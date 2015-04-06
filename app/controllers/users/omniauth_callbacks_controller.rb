f6b6c45a61072870c023407909de6a46f5474b48
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-04-06 21:51:58 +0100

    added button to user registration

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def provider
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
+    @user.update(confirmed_at: Time.now)
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
